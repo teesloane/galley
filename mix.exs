faa572483ef082f7e6de967f41cf170e9775a0cb
teesloane <tylersloane@gmail.com>
2022-04-20 11:23:23 -0700

    Clean: fmt heex.

--- mix.exs
+++ mix.exs
@@ -40,7 +40,7 @@ defmodule Galley.MixProject do
       {:postgrex, ">= 0.0.0"},
       {:phoenix_html, "~> 3.0"},
       {:phoenix_live_reload, "~> 1.2", only: :dev},
-      {:phoenix_live_view, "~> 0.17.5"},
+      {:phoenix_live_view, "~> 0.17.9"},
       {:floki, ">= 0.30.0", only: :test},
       {:phoenix_live_dashboard, "~> 0.6"},
       {:esbuild, "~> 0.3", runtime: Mix.env() == :dev},
