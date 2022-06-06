21377a3681d80aa0d1edcf1b7dc943f2db90e063
teesloane <tylersloane@gmail.com>
2022-06-05 20:16:20 -0400

    Add: HTTPoison.

--- mix.exs
+++ mix.exs
@@ -38,6 +38,7 @@ defmodule Galley.MixProject do
       {:phoenix_ecto, "~> 4.4"},
       {:ecto_sql, "~> 3.6"},
       # {:postgrex, ">= 0.0.0"},
+      {:httpoison, "~> 1.8"},
       {:ecto_sqlite3, ">= 0.0.0"},
       {:phoenix_html, "~> 3.0"},
       {:phoenix_live_reload, "~> 1.2", only: :dev},
