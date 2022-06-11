924ec9fe730262b24850bdf7386d69b9aaa635d3
teesloane <tylersloane@gmail.com>
2022-06-11 09:06:35 -0400

    Refactor: use phoenix-tailwind not npm tailwind.

--- mix.exs
+++ mix.exs
@@ -55,7 +55,8 @@ defmodule Galley.MixProject do
       {:telemetry_poller, "~> 1.0"},
       {:gettext, "~> 0.18"},
       {:jason, "~> 1.2"},
-      {:plug_cowboy, "~> 2.5"}
+      {:plug_cowboy, "~> 2.5"},
+      {:tailwind, "~> 0.1", runtime: Mix.env() == :dev}
     ]
   end
 
