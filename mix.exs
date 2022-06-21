26a111f9d6819d2eafe439979cc0b9167b2f9d21
teesloane <tylersloane@gmail.com>
2022-06-21 18:14:04 -0400

    Add: inets to extra applications.

--- mix.exs
+++ mix.exs
@@ -20,7 +20,7 @@ defmodule Galley.MixProject do
   def application do
     [
       mod: {Galley.Application, []},
-      extra_applications: [:logger, :runtime_tools]
+      extra_applications: [:logger, :runtime_tools, :inets]
     ]
   end
 
