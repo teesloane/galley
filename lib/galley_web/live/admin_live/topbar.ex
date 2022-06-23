8b04ad24933eaae6eb333b72ea314cf0f2668019
tees <tylersloane@gmail.com>
2022-06-23 07:09:53 -0400

    Add: clean up top admin panel.

--- lib/galley_web/live/admin_live/topbar.ex
+++ lib/galley_web/live/admin_live/topbar.ex
@@ -6,16 +6,15 @@ defmodule GalleyWeb.AdminLive.TopBar do
     index_links = [
       %{name: "Home", link: Routes.admin_index_path(assigns.socket, :index)},
       %{name: "Users", link: Routes.admin_users_path(assigns.socket, :show)},
-      %{name: "Recipes", link: Routes.admin_recipes_path(assigns.socket, :show)},
+      %{name: "Recipes", link: Routes.admin_recipes_path(assigns.socket, :show)}
     ]
 
     ~H"""
-    <div class="flex flex-row bg-blue-600 text-white pl-4 text-sm">
+    <div class="flex flex-row bg-blue-600 text-white pl-4 text-sm mt-10">
         <%= for l <- index_links do %>
             <%= link(l.name, to: l.link, class: "py-2 pr-4")%>
         <% end %>
     </div>
     """
   end
-
 end
