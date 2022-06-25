845f6854162452a5e0b2651e9e077fcee1cf0115
tees <tylersloane@gmail.com>
2022-06-25 14:19:50 -0400

    Test: update user fixture to have username.

--- test/support/fixtures/accounts_fixtures.ex
+++ test/support/fixtures/accounts_fixtures.ex
@@ -6,11 +6,13 @@ defmodule Galley.AccountsFixtures do
 
   def unique_user_email, do: "user#{System.unique_integer()}@example.com"
   def valid_user_password, do: "hello world!"
+  def valid_user_name, do: "user#{System.unique_integer()}"
 
   def valid_user_attributes(attrs \\ %{}) do
     Enum.into(attrs, %{
       email: unique_user_email(),
-      password: valid_user_password()
+      password: valid_user_password(),
+      username: valid_user_name()
     })
   end
 
