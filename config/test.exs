0125420b9a90d74b4a872efdfa70073d1848d68c
tees <tylersloane@gmail.com>
2022-06-25 14:19:15 -0400

    Test: get test db properly setup.
    - needed to have a `pool: Ecto.Adapters.SQL.Sandbox`

--- config/test.exs
+++ config/test.exs
@@ -9,9 +9,10 @@ config :bcrypt_elixir, :log_rounds, 1
 # to provide built-in test partitioning in CI environment.
 # Run `mix help test` for more information.
 config :galley, Galley.Repo,
-  database: Path.expand("../galleydata_dev.db", Path.dirname(__ENV__.file)),
+  database: Path.expand("../galleydata_dev_test.db", Path.dirname(__ENV__.file)),
   pool_size: 5,
-  show_sensitive_data_on_connection_error: true
+  show_sensitive_data_on_connection_error: true,
+  pool: Ecto.Adapters.SQL.Sandbox
 
 # We don't run a server during test. If one is required,
 # you can enable the server option below.
